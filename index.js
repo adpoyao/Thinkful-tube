04bfe12881676e328aa882c835a05a78fbd26e37
Eddie Yao <adpoyao@gmail.com>
2017-10-04 11:21:32 -0700

    added STATE

--- index.js
+++ index.js
@@ -9,45 +9,50 @@
 //display the result (thumbnail image) on the dom
 
 const youtubeKey = 'AIzaSyCLTW5L2hLYTbWfueyDRzzwt9vQead4PP0';
-
-
-
 const youtubeEndpoint = 'https://www.googleapis.com/youtube/v3/search';
+
+const initialState = {
+  video: [
+    // {thumbnail: 'url', title: 'title', link: 'link' }
+  ]
+};
+
+let STATE = Object.assign({}, initialState);
 
 const params = {
   part: 'snippet',
   key: youtubeKey,
   q: 'cats'
-}
+};
 $.getJSON(youtubeEndpoint, params, function(response) {
   console.log(response);
 });
 
-// function getDataFromApi(searchValue, callback) {
-//   //pass searchValue to the query
-//   //use getJSON method on the youtubeEndpoint
-//   //pass in query and the callback function to getJSON //method
-// }
+function getDataFromApi(searchValue, callback) {
+  //pass searchValue to the query
+  //use getJSON method on the youtubeEndpoint
+  //pass in query and the callback function to getJSON //method
+}
 
-// function renderSearchResult(result) {
-//   //call back function for displayThumbnailData
-//   //returning individual item to html element
-//   return `
-//   <div>
-//     <input type="image" class="js-thumbnail" src = ${result.something} target="_blank"$!VAR!><a class="js-thumbnail-url" href="${result.something}" target="_blank">
-//   </div>
-//   `;
-// }
+function renderSearchResult(result) {
+  //call back function for displayThumbnailData
+  //returning individual item to html element
+  return `
+  <div>
+    <input type="image" class="js-thumbnail" src = ${result.something} target="_blank"$!VAR!><a class="js-thumbnail-url" href="${result.something}" target="_blank">
+  </div>
+  `;
+}
 
-// function displayThumbnailData(returnedDataFromJSON){
-//   //creates a new array of all the items from JSON
-//   //and renders them to the dom
-// }
+function displayThumbnailData(returnedDataFromJSON){
+  //creates a new array of all the items from JSON
+  //and renders them to the dom
+}
 
-// function handleSubmitButton() {
-//   //listen to user submit on form
-//   //prevent the default action
-//   //store the value of input
-//   //clear the value of the search bar after search
-//   //pass the search value to the json function(getDataFromApi)
-// }
+function handleSubmitButton() {
+  //listen to user submit on form
+  //prevent the default action
+  //store the value of input
+  //clear the value of the search bar after search
+  //pass the search value to the json function(getDataFromApi)
+}
